67d9f96d5a4f64cb4e739327f22158e6c054970c
Sergio Camargo <sergio.camargo813@gmail.com>
2018-01-17 22:25:10 -0800

    norme header validations

--- grimly/main.c
+++ grimly/main.c
@@ -6,7 +6,7 @@
 /*   By: scamargo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/17 15:30:32 by scamargo          #+#    #+#             */
-/*   Updated: 2018/01/17 20:29:30 by scamargo         ###   ########.fr       */
+/*   Updated: 2018/01/17 22:03:48 by scamargo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,26 +18,25 @@ static void	grim_it(char *file_name)
 {
 	t_grim *map;
 
-	if(!(map = (t_grim*)ft_memalloc(sizeof(t_grim))))
+	if (!(map = (t_grim*)ft_memalloc(sizeof(t_grim))))
 		return ;
 	if (!map_is_valid(file_name, map))
 	{
-		write(2,"MAP ERROR\n",10);
+		write(2, "MAP ERROR\n", 10);
 		return ;
 	}
 	ft_putendl("Map valid!");
 }
 
-int	main(int ac, char **av)
+int			main(int ac, char **av)
 {
 	char *input;
-	// read arguments, else read from stdin
+
 	if (ac > 1)
 	{
 		av++;
 		while (--ac)
 		{
-			printf("av grim it: %s\n", *av);
 			grim_it(*av);
 			av++;
 		}
@@ -46,7 +45,6 @@ int	main(int ac, char **av)
 	{
 		if (!(input = ft_stdin()))
 			return (1);
-		printf("stdin grim it: %s", input); //TODO: remove
 		grim_it(input);
 	}
 	return (0);
--- grimly/validate_map.c
+++ grimly/validate_map.c
@@ -6,12 +6,13 @@
 /*   By: scamargo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/17 17:34:27 by scamargo          #+#    #+#             */
-/*   Updated: 2018/01/17 21:00:27 by scamargo         ###   ########.fr       */
+/*   Updated: 2018/01/17 22:24:19 by scamargo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "grimly.h"
+#include <fcntl.h>
 #include <stdio.h>
 
 static int	no_repeat_symbols(t_grim *map)
@@ -29,30 +30,47 @@ static int	no_repeat_symbols(t_grim *map)
 	return (1);
 }
 
-static int	header_is_valid(char *file_name, t_grim *map)
+static char	*get_header(int file_descriptor)
 {
-	char	*header;
-	//int		file_descriptor;
-	
-	ft_putendl("made it to header_is_valid");
-	ft_putendl(file_name);
-	//file_descriptor = open(READ_ONLY);
-	//header = get_next_line(file_descriptor);
-	header = "31000x31000* o12";
+	t_array	*buff;
+	char	in_buff[30];
+	int		reader;
+	char	*result;
+
+	if (!(buff = arr_init(30)))
+		return (0);
+	while ((reader = read(file_descriptor, in_buff, 29)))
+	{
+		if (reader == -1)
+			return (0);
+		in_buff[reader] = '\0';
+		reader = 0;
+		while (in_buff[reader])
+		{
+			if (in_buff[reader] == '\n')
+			{
+				result = buff->str;
+				free(buff);
+				return (result);
+			}
+			arr_insert(buff, in_buff[reader++]);
+		}
+	}
+	return (0);
+}
+
+static int	parse_header_symbols(t_grim *map, char *header)
+{
 	if ((map->lines = ft_atoi(header)) <= 0)
 		return (0);
-	while (*header++ != 'x')
-	{
-		if (!*header)
-			return (0);
-	}
+	while (*header && *header++ != 'x')
+	t 	header++;
 	if ((map->columns = ft_atoi(header++)) <= 0)
 		return (0);
 	if ((map->lines * map->columns) > 1000000000)
 		return (0);
 	while (*header >= '0' && *header <= '9')
 		header++;
-	printf("lines: %d, col: %d\n", map->lines, map->columns);
 	if (!(map->full = *header++))
 		return (0);
 	if (!(map->empty = *header++))
@@ -65,6 +83,21 @@ static int	header_is_valid(char *file_name, t_grim *map)
 		return (0);
 	if (*header != '\0')
 		return (0);
+	return (1);
+}
+
+static int	header_is_valid(int file_descriptor, t_grim *map)
+{
+	char	*header;
+
+	if (!(header = get_header(file_descriptor)))
+		return (0);
+	if (!parse_header_symbols(map, header))
+	{
+		free(header);
+		return (0);
+	}
+	free(header);
 	if (!no_repeat_symbols(map))
 		return (0);
 	return (1);
@@ -72,7 +105,12 @@ static int	header_is_valid(char *file_name, t_grim *map)
 
 int			map_is_valid(char *file_name, t_grim *map)
 {
-	if (!(header_is_valid(file_name, map)))
+	int fd;
+
+	if ((fd = open(file_name, O_RDONLY)) == -1)
 		return (0);
+	if (!(header_is_valid(fd, map)))
+		return (0);
+	close(fd);
 	return (1);
 }
